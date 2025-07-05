5eb0b08a335bb333651a7ccee543e83255154856
Sandytheking <sandytheking@hotmail.com>
2025-07-05 10:44:33 -0400

    Archivo disponibilidad editado

--- routes/disponibilidad.js
+++ routes/disponibilidad.js
@@ -1,7 +1,7 @@
-// routes/disponibilidad.js
-const express = require('express');
-const { getConfigBySlug } = require('../supabaseClient.js');
-const { getAccessToken, getEventsForDay } = require('../utils/google.js');
+// 📁 routes/disponibilidad.js         
+import express from 'express';
+import { getConfigBySlug }      from '../supabaseClient.js';
+import { getAccessToken, getEventsForDay }      from '../utils/google.js';
 
 const router = express.Router();
 
