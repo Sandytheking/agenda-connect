b09b38c949f275202a054bd2c18e86d57444478a
Sandytheking <sandytheking@hotmail.com>
2025-07-10 19:15:51 -0400

    Update

--- utils/helpers.js
+++
@@ -1,9 +0,0 @@
-// utils/helpers.js  (pequeño helper reutilizable)
-export function toLocalDateTime(dateStr, timeStr) {
-  // dateStr = 'YYYY-MM-DD'   |   timeStr = 'HH:mm'
-  const [y, m, d]   = dateStr.split('-').map(Number);
-  const [hh, mm]    = timeStr.split(':').map(Number);
-  const jsDate      = new Date(y, m - 1, d, hh, mm, 0, 0); // ← *local* en el server
-  const isoNoZ      = `${dateStr}T${timeStr}:00`;          // para Google
-  return { jsDate, isoNoZ };
-}
