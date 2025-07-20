4595a94e605618fdfe84fad6322faddbe0be320b
Sandytheking <sandytheking@hotmail.com>
2025-07-19 21:39:08 -0400

    update

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
+export const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
 
 export async function getConfigBySlug(slug) {
   const { data, error } = await supabase
