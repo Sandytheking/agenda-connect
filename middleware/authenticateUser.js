d0cb22816bb2bfdc9a3213ef28207430f9f51759
Sandytheking <sandytheking@hotmail.com>
2025-08-03 11:14:39 -0400

    update

--- middleware/authenticateUser.js
+++ middleware/authenticateUser.js
@@ -1,9 +1,13 @@
 // middlewares/authenticateUser.js
 import { createClient } from '@supabase/supabase-js';
 
+if (!process.env.SUPABASE_SERVICE_ROLE_KEY || !process.env.SUPABASE_URL) {
+  throw new Error('❌ Variables SUPABASE_SERVICE_ROLE_KEY o SUPABASE_URL no están definidas');
+}
+
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 export async function authenticateUser(req, res, next) {
